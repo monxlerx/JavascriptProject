6ffbdbaee5d2a8366ae0082907adba6a9c5bb82f
Conquer The World <conquertheworld@MacBook-Pro-de-Conquer.local>
2020-09-19 14:29:46 +0200

    cuarto commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,49 @@
 
+//API
+const URL = 'https://raw.githubusercontent.com/ITBconsult/json-data/master/data.json';
+
+// create an element
+const createNode = (elem) => {
+    return document.createElement(elem);
+};
+
+// append an element to parent
+const appendNode = (parent, elem) => {
+    parent.appendChild(elem);
+}
+
+//Get all users from the API
+
+// List Element
+const ul = document.querySelector('#users');
+
+// make the API call
+fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        // iterate over users
+        data.map((user) => {
+            // create the elements
+            let li = createNode('li'),
+                p1 = createNode('p'),
+                p2 = createNode('p');
+            p1.innerText = user.name;
+            p2.innerText = user.city;
+            // append all elements
+            appendNode(li, p1);
+            appendNode(li, p2);
+            appendNode(ul, li);
+        });
+    }).catch(err => {
+        console.error('Error: ', err);
+    });
+
+
+
+
+
+
+
 function retrieveUsers() {
     fetch(`https://raw.githubusercontent.com/ITBconsult/json-data/master/data.json`)
         .then(function (response) {
@@ -11,6 +56,11 @@ function retrieveUsers() {
             console.error(err);
         });
 }
+
+window.addEventListener('DOMContentLoaded', retrieveUsers, false);
+
+
+//Create a GitHub user
 
 function addUser(user) {
     fetch('https://raw.githubusercontent.com/ITBconsult/json-data/master/data.json', {
