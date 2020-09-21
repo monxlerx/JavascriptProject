e82e1bf5996c3937f97054f5fb2d48f95e7ae991
Conquer The World <conquertheworld@MacBook-Pro-de-Conquer.local>
2020-09-21 22:37:53 +0200

    noveno commit

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ function getUser(id) {
 
 //Create a GitHub user through form
 let btnAddUser = document.getElementById("btnAddUser");
-if(btnAddUser) {
+if (btnAddUser) {
     btnAddUser.addEventListener('click', addUser, false);
 }
 
@@ -125,6 +125,38 @@ function restoreBackgroundColor(row) {
     row.style.backgroundColor = backgroundMouseOutColor;
 }
 
+//Search a user by Name
+function searchUsers() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+//Print pagination 
+let pagination = document.getElementById("pagination");
+
+function pagination(resultsPerPage, totalResults) {
+    for(let index = 1; )
+
+}
+
+function countingUsers() {
+
+}
+
 function sortByName() {
 
 }
@@ -134,6 +166,86 @@ function sortByCity() {
 }
 
 
+// Pagination function
 
+const list_items = [
+	"Item 1",
+	"Item 2",
+	"Item 3",
+	"Item 4",
+	"Item 5",
+	"Item 6",
+	"Item 7",
+	"Item 8",
+	"Item 9",
+	"Item 10",
+	"Item 11",
+	"Item 12",
+	"Item 13",
+	"Item 14",
+	"Item 15",
+	"Item 16",
+	"Item 17",
+	"Item 18",
+	"Item 19",
+	"Item 20",
+	"Item 21",
+	"Item 22"
+];
 
+const list_element = document.getElementById('list');
+const pagination_element = document.getElementById('pagination');
 
+let current_page = 1;
+let rows = 5;
+
+function DisplayList (items, wrapper, rows_per_page, page) {
+	wrapper.innerHTML = "";
+	page--;
+
+	let start = rows_per_page * page;
+	let end = start + rows_per_page;
+	let paginatedItems = items.slice(start, end);
+
+	for (let i = 0; i < paginatedItems.length; i++) {
+		let item = paginatedItems[i];
+
+		let item_element = document.createElement('div');
+		item_element.classList.add('item');
+		item_element.innerText = item;
+		
+		wrapper.appendChild(item_element);
+	}
+}
+
+function SetupPagination (items, wrapper, rows_per_page) {
+	wrapper.innerHTML = "";
+
+	let page_count = Math.ceil(items.length / rows_per_page);
+	for (let i = 1; i < page_count + 1; i++) {
+		let btn = PaginationButton(i, items);
+		wrapper.appendChild(btn);
+	}
+}
+
+function PaginationButton (page, items) {
+	let button = document.createElement('button');
+	button.innerText = page;
+
+	if (current_page == page) button.classList.add('active');
+
+	button.addEventListener('click', function () {
+		current_page = page;
+		DisplayList(items, list_element, rows, current_page);
+
+		let current_btn = document.querySelector('.pagenumbers button.active');
+		current_btn.classList.remove('active');
+
+		button.classList.add('active');
+	});
+
+	return button;
+}
+
+DisplayList(list_items, list_element, rows, current_page);
+SetupPagination(list_items, pagination_element, rows);
